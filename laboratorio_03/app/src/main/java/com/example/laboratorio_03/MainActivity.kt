ddabb8ae50793658aff4df85ca31840f120df37f
rodolfo <00082421@uca.edu.sv>
2023-03-28 12:22:00 -0600

    labo_03

---
+++ laboratorio_03/app/src/main/java/com/example/laboratorio_03/MainActivity.kt
@@ -0,0 +1,16 @@
+package com.example.laboratorio_03
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    //private lateinit var resultTextView: TextView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+
+}

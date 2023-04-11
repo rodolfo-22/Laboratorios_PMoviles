834dd38a0bf13e64fd81d2844a23e62fd58457bd
rodolfo <00082421@uca.edu.sv>
2023-04-11 12:42:12 -0600

    labo_04

---
+++ laboratorio_04/app/src/main/java/com/example/laboratorio_04/MainActivity.kt
@@ -0,0 +1,22 @@
+package com.example.laboratorio_04
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+    }
+
+
+
+    /*val intent = Intent(this, activity_main2::class.java)
+    intent.putExtra("key","value")
+    startActivity(intent)*/
+}

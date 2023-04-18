ede38128791d172afb641e1f32459c8b361f5fb5
rodolfo <00082421@uca.edu.sv>
2023-04-18 12:59:00 -0600

    adding_labo_05

---
+++ Labo_05/app/src/main/java/com/example/labo_05/Form_new_movie.kt
@@ -0,0 +1,11 @@
+package com.example.labo_05
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Form_new_movie : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_form_new_movie)
+    }
+}

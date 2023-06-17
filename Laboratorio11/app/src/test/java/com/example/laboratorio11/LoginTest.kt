a244a4546d936f097aed3c002371cf2d0ce83ca0
rodolfo <00082421@uca.edu.sv>
2023-06-16 19:16:08 -0600

    Adding  labo 12 testing login:sparkles:

---
+++ Laboratorio11/app/src/test/java/com/example/laboratorio11/LoginTest.kt
@@ -0,0 +1,4 @@
+package com.example.laboratorio11
+
+class LoginTest {
+}
---
+++ Laboratorio12/app/src/main/java/com/example/laboratorio11/network/dto/login/LoginRequest.kt
@@ -0,0 +1,6 @@
+package com.example.laboratorio11.network.dto.login
+
+class LoginRequest (
+    val email: String,
+    val password: String
+)
